249f74fdb2425d6bde5394ba614417daffe84528
mrcpc <95518394+mrcpc@users.noreply.github.com>
2022-05-23 19:55:30 -0400

    Add files via upload
    :)

--- project 3.sol
+++ project 3.sol
@@ -1,62 +1,66 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 // Imports 
 
-import "@openzeppelin/contracts/finance/VestingWallet.sol";
+//import "@openzeppelin/contracts/finance/VestingWallet.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 //Contract Creation
 
 contract RecordLabel {
-    string ARTIST = //streamlit input;
-    string ARTIST_FULL_NAME = //streamlit input;
-    string RECORD_COMPANY = //streamlit input;
-    uint INITIAL_PAYMENT = 
-    string CITY_AND_STATE_OF_LAW = 
-    string ESIGNATURE_OF_ARTIST = 
-    string ESIGNATURE_OF_RECORD_COMPANY = 
-    address payable RECORD_COMPANY_WALLET_ADDRESS = 
-    address payable ARTIST_WALLET_ADDRESS = 
-    uint COST_OF_FULL_OWNERSHIP = 
-    uint PERCENTAGE_OF_GROSS_REVENUE = 
-    uint PERCENTAGE_OF_ROYALTIES_TO_COMPANY = 
-    uint PERCENTAGE_OF_ROYALTIES_TO_ARTIST = 
-    bool EXCLUSIVE_OR_PARTIAL_LICENSE = 
-    uint PERCENTAGE_OF_RIGHTS_WILLING_TO_ASSIGN_TO_THE_RECORD_LABEL = 
-    uint NUMBER_OF_BODY_OF_WORKS = 
-    uint NUMBER_OF_YEARS = 
-    uint DATE = 
+    string ARTIST = "Kendrick Lamar"; //streamlit input;
+    string ARTIST_FULL_NAME = "Kendrick Lamar Duckworth"; //streamlit input;
+    string RECORD_COMPANY = "pgLang";//streamlit input;
+    uint INITIAL_PAYMENT = 2500; 
+    string CITY_AND_STATE_OF_LAW = "Los Angeles, CA"; 
+    string ESIGNATURE_OF_ARTIST = "K-Dot";
+    string ESIGNATURE_OF_RECORD_COMPANY = "pgLang Signature";
+    address payable RECORD_COMPANY_WALLET_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; 
+    address payable ARTIST_WALLET_ADDRESS = 0x4BB3e061A546528A44F2B973906267C4EDa5A49F;
+    uint COST_OF_FULL_OWNERSHIP = 10000;
+    uint PERCENTAGE_OF_GROSS_REVENUE = 30;
+    uint PERCENTAGE_OF_ROYALTIES_TO_COMPANY = 50;
+    uint PERCENTAGE_OF_ROYALTIES_TO_ARTIST = 50;
+    bool EXCLUSIVE_OR_PARTIAL_LICENSE = true;
+    uint PERCENTAGE_OF_RIGHTS_WILLING_TO_ASSIGN_TO_THE_RECORD_LABEL = 50;
+    uint NUMBER_OF_BODY_OF_WORKS = 3;
+    uint NUMBER_OF_YEARS = 5;
+    string DATE = "June 1, 2022";
 }
 
+
 // Set up logic for payment disbursements
-constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
+//constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
+   
+
+//receive()
+
+//address beneficiary()
+
+//uint256 start()
+
+//uint256 duration()
+
+//uint256 released(address, token)
+
+//released(token)
+
+//release()
+
+//release(token)
+
+//vestedAmount(timestamp)
+//vestedAmount(token, timestamp)
+
+//_vestingSchedule(totalAllocation, timestamp)
 
 
-receive()
-
-beneficiary()
-
-start()
-
-duration()
-
-released(to, amount)
-
-released(token)
-
-release()
-
-release(token)
-
-vestedAmount(timestamp)
-
-vestedAmount(token, timestamp)
-
-_vestingSchedule(totalAllocation, timestamp)
 
 
-PayeeAdded(account, shares)
-
-
+ 
 
 //function ContractInit {
    
@@ -78,9 +82,9 @@ contract VestingWallet is Context {
      * @dev Set the beneficiary, start timestamp and vesting duration of the vesting wallet.
      */
     constructor(
-        address beneficiaryAddress==ARTIST_WALLET_ADDRESS,
-        uint64 startTimestamp==DATE,
-        uint64 durationSeconds==NUMBER_OF_YEARS
+        address beneficiaryAddress,
+        uint64 startTimestamp,
+        uint64 durationSeconds
     ) {
         require(beneficiaryAddress != address(0), "VestingWallet: beneficiary is zero address");
         _beneficiary = beneficiaryAddress;
@@ -119,7 +123,7 @@ contract VestingWallet is Context {
      * @dev Amount of eth already released
      */
     function released() public view virtual returns (uint256) {
-        return _released==INITIAL_PAYMENT;
+        return _released;
     }
 
     /**
@@ -147,7 +151,7 @@ contract VestingWallet is Context {
      * Emits a {ERC20Released} event.
      */
     function release(address token) public virtual {
-        uint256 releasable = vestedAmount(token, uint64(block.timestamp)) - released(token);
+        uint256 releasable = vestedAmount( token, uint64(block.timestamp)) - released(); //token
         _erc20Released[token] += releasable;
         emit ERC20Released(token, releasable);
         SafeERC20.safeTransfer(IERC20(token), beneficiary(), releasable);
@@ -165,15 +169,15 @@ contract VestingWallet is Context {
      * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
      decide if we want to keep as tokens
      */
-    function vestedAmount(address token, uint64 timestamp) public view virtual returns (uint256) {
-        return _vestingSchedule(IERC20(token).balanceOf(address(this)) + released(token), timestamp);
+   function vestedAmount(address token, uint64 timestamp) public view virtual returns (uint256) {
+       return _vestingSchedule(IERC20(token).balanceOf(address(this)) + released(), timestamp);
     }
 
     /**
      * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
      * an asset given its total historical allocation.
      */
-    function _vestingSchedule(uint256 totalAllocation==, uint64 timestamp) internal view virtual returns (uint256) {
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view virtual returns (uint256) {
         if (timestamp < start()) {
             return 0;
         } else if (timestamp > start() + duration()) {
