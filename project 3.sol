7bf42f2fecda02be735036003b034741863438f9
Joey H <jhagemann515@gmail.com>
2022-05-25 21:19:11 -0400

    adding project 3 progress and two new templates

--- project 3.sol
+++ project 3.sol
@@ -1,54 +1,69 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 // Imports 
 
-//import "@openzeppelin/contracts/finance/VestingWallet.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/utils/SafeERC20.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 //Contract Creation
 
-contract RecordLabel {
-    string ARTIST = "Kendrick Lamar"; //streamlit input;
+contract RecordLabel is VestingWallet {
+    // Constructor Creation
+    
+    constructor() public VestingWallet(0xF5FBe792dCE8e2d1a79A532aE272a82d164271cF, 1653355311, 157784630) {}
+    receive() external payable virtual {}
+    
+    // VestingWallet()
+    // 0xF5FBe792dCE8e2d1a79A532aE272a82d164271cF, 1653355311, 157784630
+    
+    // Hard Coded above ^ but can also open sidebar input with parameters in first constructor
+    //constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
+    
+    // -------------------------------------------------------------------------
+    
+    // Label Deal Variable Creation
+
     string ARTIST_FULL_NAME = "Kendrick Lamar Duckworth"; //streamlit input;
     string RECORD_COMPANY = "pgLang";//streamlit input;
-    uint INITIAL_PAYMENT = 2500; 
-    string CITY_AND_STATE_OF_LAW = "Los Angeles, CA"; 
-    string ESIGNATURE_OF_ARTIST = "K-Dot";
-    string ESIGNATURE_OF_RECORD_COMPANY = "pgLang Signature";
-    address payable RECORD_COMPANY_WALLET_ADDRESS = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4; 
-    address payable ARTIST_WALLET_ADDRESS = 0x4BB3e061A546528A44F2B973906267C4EDa5A49F;
-    uint COST_OF_FULL_OWNERSHIP = 10000;
-    uint PERCENTAGE_OF_GROSS_REVENUE = 30;
-    uint PERCENTAGE_OF_ROYALTIES_TO_COMPANY = 50;
-    uint PERCENTAGE_OF_ROYALTIES_TO_ARTIST = 50;
-    bool EXCLUSIVE_OR_PARTIAL_LICENSE = true;
-    uint PERCENTAGE_OF_RIGHTS_WILLING_TO_ASSIGN_TO_THE_RECORD_LABEL = 50;
-    uint NUMBER_OF_BODY_OF_WORKS = 3;
+    // uint INITIAL_PAYMENT = 2500; 
+    // string CITY_AND_STATE_OF_LAW = "Los Angeles, CA"; 
+    // string ESIGNATURE_OF_ARTIST = "K-Dot";
+    // string ESIGNATURE_OF_RECORD_COMPANY = "pgLang Signature";
+    address RECORD_COMPANY_WALLET_ADDRESS = 0xF5FBe792dCE8e2d1a79A532aE272a82d164271cF; 
+    address ARTIST_WALLET_ADDRESS = 0xF5FBe792dCE8e2d1a79A532aE272a82d164271cF;
+    // uint COST_OF_FULL_OWNERSHIP = 10000;
+    // uint PERCENTAGE_OF_GROSS_REVENUE = 30;
+    // uint PERCENTAGE_OF_ROYALTIES_TO_COMPANY = 50;
+    // uint PERCENTAGE_OF_ROYALTIES_TO_ARTIST = 50;
+    // bool EXCLUSIVE_OR_PARTIAL_LICENSE = true;
+    // uint PERCENTAGE_OF_RIGHTS_WILLING_TO_ASSIGN_TO_THE_RECORD_LABEL = 50;
+    // uint NUMBER_OF_BODY_OF_WORKS = 3;
     uint NUMBER_OF_YEARS = 5;
     string DATE = "June 1, 2022";
-}
+    
 
-
-// Set up logic for payment disbursements
-//constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds)
+// Payment Disbursment Logic
    
+// Any Arguments required such as address, or opens contract to recieve ETH?
+// receive() 
+// is this run at startup ONLY? Or does it go below with other functions?
 
-//receive()
+// beneficiary(ARTIST_WALLET_ADDRESS);
 
-//address beneficiary()
+// start(DATE);
 
-//uint256 start()
-
-//uint256 duration()
+// duration(NUMBER_OF_YEARS);
 
 //uint256 released(address, token)
 
 //released(token)
 
-//release()
+release()
 
 //release(token)
 
@@ -57,133 +72,8 @@ contract RecordLabel {
 
 //_vestingSchedule(totalAllocation, timestamp)
 
-
-
-
- 
-
 //function ContractInit {
    
-   
-    //beneficiary(ARTIST, ARTIST_WALLET_ADDRESS)
+   //beneficiary(ARTIST, ARTIST_WALLET_ADDRESS)
 //};
-
-contract VestingWallet is Context {
-    event EtherReleased(uint256 amount);
-    event ERC20Released(address indexed token, uint256 amount);
-
-    uint256 private _released;
-    mapping(address => uint256) private _erc20Released;
-    address private immutable _beneficiary;
-    uint64 private immutable _start;
-    uint64 private immutable _duration;
-
-    /**
-     * @dev Set the beneficiary, start timestamp and vesting duration of the vesting wallet.
-     */
-    constructor(
-        address beneficiaryAddress,
-        uint64 startTimestamp,
-        uint64 durationSeconds
-    ) {
-        require(beneficiaryAddress != address(0), "VestingWallet: beneficiary is zero address");
-        _beneficiary = beneficiaryAddress;
-        _start = startTimestamp;
-        _duration = durationSeconds;
-    }
-
-    /**
-     * @dev The contract should be able to receive Eth.
-     */
-    receive() external payable virtual {}
-
-    /**
-     * @dev Getter for the beneficiary address.
-     */
-    function beneficiary() public view virtual returns (address) {
-        return _beneficiary;
-    }
-
-    /**
-     * @dev Getter for the start timestamp.
-     */
-    function start() public view virtual returns (uint256) {
-        return _start;
-    }
-
-    /**
-     * @dev Getter for the vesting duration.
-     */
-    function duration() public view virtual returns (uint256) {
-        return _duration;
-    //need conditional logic 
-    }
-
-    /**
-     * @dev Amount of eth already released
-     */
-    function released() public view virtual returns (uint256) {
-        return _released;
-    }
-
-    /**
-     * @dev Amount of token already released
-     */
-    /* function released(address token) public view virtual returns (uint256) {
-        return _erc20Released[token];
-    }
-
-    /**
-     * @dev Release the native token (ether) that have already vested.
-     *
-     * Emits a {EtherReleased} event.
-     */
-    function release() public virtual {
-        uint256 releasable = vestedAmount(uint64(block.timestamp)) - released();
-        _released += releasable;
-        emit EtherReleased(releasable);
-        Address.sendValue(payable(beneficiary()), releasable);
-    }
-
-    /**
-     * @dev Release the tokens that have already vested.
-     *
-     * Emits a {ERC20Released} event.
-     */
-    function release(address token) public virtual {
-        uint256 releasable = vestedAmount( token, uint64(block.timestamp)) - released(); //token
-        _erc20Released[token] += releasable;
-        emit ERC20Released(token, releasable);
-        SafeERC20.safeTransfer(IERC20(token), beneficiary(), releasable);
-    }
-
-    /**
-     * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
-     change from linear; make into block payments by conditions 
-     */
-    function vestedAmount(uint64 timestamp) public view virtual returns (uint256) {
-        return _vestingSchedule(address(this).balance + released(), timestamp);
-    }
-
-    /**
-     * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
-     decide if we want to keep as tokens
-     */
-   function vestedAmount(address token, uint64 timestamp) public view virtual returns (uint256) {
-       return _vestingSchedule(IERC20(token).balanceOf(address(this)) + released(), timestamp);
-    }
-
-    /**
-     * @dev Virtual implementation of the vesting formula. This returns the amount vested, as a function of time, for
-     * an asset given its total historical allocation.
-     */
-    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view virtual returns (uint256) {
-        if (timestamp < start()) {
-            return 0;
-        } else if (timestamp > start() + duration()) {
-            return totalAllocation;
-        } else {
-            return (totalAllocation * (timestamp - start())) / duration();
-        }
-    }
 }
